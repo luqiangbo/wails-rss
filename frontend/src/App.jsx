b3360918445ac5e6dc4749cf6469d046938d1355
luqiangbo <luqiangbo@orionstar.com>
2024-04-29 15:47:16 +0800

    tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
 import { Button, Input, Row, Col } from 'antd'
 
-import './App.less'
+import './styles/App.less'
 
 function App() {
   return (
     <div id='App'>
       <Row>
         <Col span={6}>
-          <div className='p-3'>
+          <div className='p-2'>
             <Input placeholder='Please Input' />
           </div>
         </Col>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { ConfigProvider, theme } from 'antd'
 
-import './tailwind.css'
+import './styles/tailwind.css'
 import App from './App'
 
 const container = document.getElementById('root')
